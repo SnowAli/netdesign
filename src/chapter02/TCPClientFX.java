f1938e5fbedcdfe9d9b107a53a80f40e69153b6b
Ean <958259016@qq.com>
2021-09-13 12:01:38 +0800

    chapter02:tcp网络通信

--- src/chapter02/TCPClientFX.java
+++ src/chapter02/TCPClientFX.java
@@ -1,6 +1,5 @@
 package chapter02;
 
-import chapter01.TextFileIO;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -19,7 +18,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
+
 
 public class SimpleFX extends Application {
   private TextField ipInput = new TextField();
@@ -28,13 +27,46 @@ public class SimpleFX extends Application {
   private Button btnConnect = new Button("连接");
   private Button btnExit = new Button("退出");
   private Button btnSend = new Button("发送");
-  private Button btnOpen = new Button("加载");
-  private Button btnSave = new Button("保存");
 
   private TextField sendInput = new TextField();
   private TextArea infoDisplay = new TextArea();
 
   private TCPClient tcpClient;
+
+  private void openConnect() {
+      String ip = ipInput.getText().trim();
+      String port = portInput.getText().trim();
+      try {
+          tcpClient = new TCPClient(ip, port);
+          String firstMsg = tcpClient.receive();
+          infoDisplay.appendText(firstMsg + "\n");
+          btnSend.setDisable(false);
+          btnConnect.setDisable(true);
+      } catch (Exception e) {
+          infoDisplay.appendText("服务器连接失败！" + e.getMessage() + "\n");
+      }
+  }
+  private void closeConnect() {
+      if(tcpClient != null) {
+          tcpClient.send("bye");
+          tcpClient.close();
+      }
+  };
+  private void sendMessage() {
+      String sendMsg = sendInput.getText();
+//      infoDisplay.appendText(msg + "\n");
+//      sendInput.clear();
+      if(tcpClient != null) {
+          tcpClient.send(sendMsg);
+          infoDisplay.appendText("客户端发送：" + sendMsg + "\n");
+          if(sendMsg.equals("bye")) {
+              btnSend.setDisable(true);
+              btnConnect.setDisable(false);
+          }
+          String receiveMsg = tcpClient.receive();
+          infoDisplay.appendText(receiveMsg + "\n");
+      }
+  };
 
   public SimpleFX() throws IOException {
   }
@@ -44,7 +76,7 @@ public class SimpleFX extends Application {
     BorderPane mainPane = new BorderPane();
 
     VBox vBox = new VBox();
-    vBox.setSpacing(10); // spacing of the components
+    vBox.setSpacing(10);
     vBox.setPadding(new Insets(10, 20, 10, 20));
 
 
@@ -60,72 +92,34 @@ public class SimpleFX extends Application {
 
     VBox.setVgrow(infoDisplay, Priority.ALWAYS);
 
-   btnConnect.setOnAction(event -> {
-     String ip = ipInput.getText().trim();
-     String port = portInput.getText().trim();
-     try {
-       tcpClient = new TCPClient(ip, port);
-       String firstMsg = tcpClient.receive();
-       infoDisplay.appendText(firstMsg + "\n");
-     } catch (Exception e) {
-       infoDisplay.appendText("服务器连接失败！" + e.getMessage() + "\n");
-     }
-   });
+    btnConnect.setOnAction(event -> {
+        openConnect();
+    });
 
+    btnSend.setDisable(true);
     btnSend.setOnAction(event -> {
-      String sendMsg = sendInput.getText();
-//      infoDisplay.appendText(msg + "\n");
-//      sendInput.clear();
-      if(tcpClient != null) {
-        tcpClient.send(sendMsg);
-        infoDisplay.appendText("客户端发送：" + sendMsg + "\n");
-        String receiveMsg = tcpClient.receive();
-        infoDisplay.appendText(receiveMsg + "\n");
-      }
+        sendMessage();
     });
 
-    TextFileIO textFileIO = new TextFileIO();
-    btnSave.setOnAction(event -> {
-      textFileIO.append(
-              LocalDateTime.now().withNano(0) + " " + infoDisplay.getText()
-      );
-    });
-    btnOpen.setOnAction(event -> {
-      String msg = textFileIO.load();
-      if(msg != null){
-        infoDisplay.clear();
-        infoDisplay.setText(msg);
-      }
-    });
 
     sendInput.setOnKeyPressed(new EventHandler<KeyEvent>() {
       @Override
       public void handle(KeyEvent event) {
         if(event.getCode() == KeyCode.ENTER) {
-          String msg = sendInput.getText();
-          if(event.isShiftDown()) { // isShiftDown方法判断shift键是按下
-            infoDisplay.appendText("echo:" + msg + "\n");
-          } else {
-            infoDisplay.appendText(msg + "\n");
-          }
-          sendInput.clear();
+         sendMessage();
         };
       }
     });
 
     btnExit.setOnAction(event -> {
-      if(tcpClient != null) {
-        tcpClient.send("bye");
-        tcpClient.close();
-      }
-      System.exit(0);
+        closeConnect();
     });
 
     HBox btnBox = new HBox();
     btnBox.setSpacing(10);
     btnBox.setPadding(new Insets(10,20,10,20));
     btnBox.setAlignment(Pos.CENTER_RIGHT);
-    btnBox.getChildren().addAll(btnSend, btnSave, btnOpen, btnExit);
+    btnBox.getChildren().addAll(btnSend, btnExit);
 
     mainPane.setCenter(vBox);
     mainPane.setBottom(btnBox);
@@ -135,6 +129,11 @@ public class SimpleFX extends Application {
 
     primaryStage.setScene(scene);
     primaryStage.show();
+
+    primaryStage.setOnCloseRequest(event -> {
+        tcpClient.close();
+    });
+
   }
 }
 
